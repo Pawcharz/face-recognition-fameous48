17b7b2c7794fc9fc7c8a1fc5a410b35c1f600b94
polemaster <kuba.czermanski@protonmail.com>
2024-05-13 19:31:10 +0200

    Update file loading and model creation

--- utils/load_dataset.py
+++ utils/load_dataset.py
@@ -1,71 +1,54 @@
-from sklearn.model_selection import train_test_split
-import re
 import numpy as np
+from sklearn.model_selection import train_test_split
 
-from utils.constrains import CLASSES, IMAGE_SIZE
+from utils.constants import CLASSES, IMAGE_SIZE
+
+
+def read_file(filename):
+    with open(filename, 'r') as file:
+        file.readline() # we skip the first line as it is not needed
+        number_of_pixels = int(file.readline())
+
+        features = []
+        labels = []
+
+        for line in file.readlines():
+            elements = line.split()
+
+        # add features
+        pixels = np.array(elements[:number_of_pixels], dtype=float)
+        pixels = np.reshape(pixels, [IMAGE_SIZE, IMAGE_SIZE])
+        features.append(pixels)
+
+        # add labels
+        labels.append(elements[number_of_pixels+2])
+
+        features = np.array(features)
+        labels = np.array(labels, dtype=int)
+
+    return features, labels
+
 
 def load_dataset(directory_path):
+    X_0, y_0 = read_file('./data/x24x24.txt')
+    X_1, y_1 = read_file('./data/y24x24.txt')
+    X_2, y_2 = read_file('./data/z24x24.txt')
 
-  def read_file(filename):
+    X = np.concatenate((X_0, X_1, X_2))
+    y = np.concatenate((y_0, y_1, y_2))
 
-    with open(filename, 'r') as file:
-      lines = file.readlines()
+    X_train, X_test, y_train_raw, y_test_raw = train_test_split(X, y,
+                                                    train_size=N_TRAIN_EXAMPLES,
+                                                    test_size=N_TEST_EXAMPLES,
+                                                    random_state=42)
 
-      # Remove newLines
-      for i, line in enumerate(lines):
-        lines[i] = line.replace('\n', '')
+    y_train = np.zeros((y_train_raw.shape[0], CLASSES))
+    y_test = np.zeros((y_test_raw.shape[0], CLASSES))
 
-      # We assume these are integers
-      EXAMPLES_NR = int(lines[0])
-      PIXELS_NR = int(lines[1])
+    for i, value in enumerate(y_train_raw):
+        y_train[i][value] = 1
 
-      inputs = list()
-      labels = np.zeros(EXAMPLES_NR, dtype=int)
+    for i, value in enumerate(y_test_raw):
+        y_test[i][value] = 1
 
-      examples_raw = lines[2:EXAMPLES_NR+2]
-
-      for i, example_raw in enumerate(examples_raw):
-        # Split by spaces (treats multiple as one)
-        tokens = re.split('\s+', example_raw)
-
-        pixel_values = np.array(tokens[0:PIXELS_NR])
-        attributes = tokens[PIXELS_NR:]
-
-        pixel_values = np.array(pixel_values, dtype=float)
-        pixel_values = pixel_values.reshape([IMAGE_SIZE, IMAGE_SIZE])
-
-        inputs.append(pixel_values)
-        labels[i] = int(attributes[2])
-
-      inputs = np.array(inputs)
-    return inputs, labels
-
-  # classes 0-15
-  X_0, y_0 = read_file(f'{directory_path}/x24x24.txt')
-  # classes 16-31
-  X_1, y_1 = read_file(f'{directory_path}/y24x24.txt')
-  # # classes 32-48
-  X_2, y_2 = read_file(f'{directory_path}/z24x24.txt')
-
-  # Concatenate train and test images
-  X = np.concatenate((X_0, X_1, X_2))
-  y = np.concatenate((y_0, y_1, y_2))
-
-  N_TRAIN_EXAMPLES=int(len(X) * 0.8)
-  N_TEST_EXAMPLES=len(X) - N_TRAIN_EXAMPLES
-
-
-  X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=N_TRAIN_EXAMPLES, test_size=N_TEST_EXAMPLES, random_state=1)
-
-  # One-Hot encoding
-  # Getting dummy variables
-  y_train_fixed = np.zeros((y_train.shape[0], CLASSES))
-  y_test_fixed = np.zeros((y_test.shape[0], CLASSES))
-
-  for i, value in enumerate(y_train):
-    y_train_fixed[i][value] = 1
-
-  for i, value in enumerate(y_test):
-    y_test_fixed[i][value] = 1
-
-  return X_train, X_test, y_train_fixed, y_test_fixed
+    return X_train, X_test, y_train, y_test
